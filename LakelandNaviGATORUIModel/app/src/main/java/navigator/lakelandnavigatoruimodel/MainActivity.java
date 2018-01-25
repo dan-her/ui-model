d91f9c2753049b86ecc46f06c95c4b49b09da69a
Hub0t <danhercik911@gmail.com>
2018-01-25 13:49:59 -0500

    new garbage
    fresh off the presses

--- LakelandNaviGATORUIModel/app/src/main/java/navigator/lakelandnavigatoruimodel/MainActivity.java
+++ LakelandNaviGATORUIModel/app/src/main/java/navigator/lakelandnavigatoruimodel/MainActivity.java
@@ -3,11 +3,20 @@ package navigator.lakelandnavigatoruimodel;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.options_menu, menu);
+
+        return true;
     }
 }
