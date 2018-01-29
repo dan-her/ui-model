c5af1c04f347696a69c74c934e44f4c8f70d26e3
Hub0t <danhercik911@gmail.com>
2018-01-29 09:48:26 -0500

    Finihed search setup
    can add database to search now

--- LakelandNaviGATORUIModel/app/src/main/java/navigator/lakelandnavigatoruimodel/MainActivity.java
+++ LakelandNaviGATORUIModel/app/src/main/java/navigator/lakelandnavigatoruimodel/MainActivity.java
@@ -1,10 +1,16 @@
 package navigator.lakelandnavigatoruimodel;
 
+import android.app.Activity;
+
+import android.app.SearchManager;
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.widget.SearchView;
 
 public class MainActivity extends AppCompatActivity {
     @Override
@@ -16,6 +22,9 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.options_menu, menu);
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView = (SearchView) menu.findItem(R.id.search).getActionView();
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
 
         return true;
     }
